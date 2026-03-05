3089cebb10dd8ef39d30cedd4f14d1034ebb39a8
Arathy <arathyrajeesh2@gmail.com>
2026-03-05 12:41:49 +0530

    name added

--- greenloop_backend/pickup/serializers.py
+++ greenloop_backend/pickup/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 from .models import Pickup
 
-
 class PickupSerializer(serializers.ModelSerializer):
+
+    resident_name = serializers.CharField(source='resident.username', read_only=True)
 
     class Meta:
         model = Pickup
-        fields = '__all__'
+        fields = [
+            'id',
+            'resident',
+            'resident_name',
+            'waste_type',
+            'address',
+            'date',
+            'status',
+            'created_at'
+        ]
         read_only_fields = ['resident']
--- greenloop_backend/users/views.py
+++ greenloop_backend/users/views.py
@@ -9,7 +9,7 @@ from rest_framework.permissions import IsAuthenticated
 from .models import User
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from .models import Pickup
+from pickup.models import Pickup
 from pickup.serializers import PickupSerializer
 
 def get_tokens(user):
